879c706612e839d6503b6e5cc6916eb87e41248a
CarrKnight <carrknight@gmail.com>
2014-11-04 16:12:37 +0800

    added small cross-section test

--- test/model/tools/inventoryTest.dart
+++ test/model/tools/inventoryTest.dart
@@ -24,5 +24,22 @@ main(){
 
   });
 
+  test("Simple receive on crossSection",(){
+
+    var totalInventory = new Inventory();
+    OneGoodInventory tested = new InventoryCrossSection(totalInventory,"pippo");
+
+    tested.receive(100.0);
+    tested.earn(200.0);
+    tested.spend(50.0);
+    tested.remove(50.0);
+    expect(totalInventory.hasHowMuch("pippo"),50.0);
+    expect(totalInventory.hasHowMuch("money"),150.0);
+
+    expect(tested.good,50.0);
+    expect(tested.money,150.0);
+    expect(tested.goodType,"pippo");
+  });
+
 
 }
