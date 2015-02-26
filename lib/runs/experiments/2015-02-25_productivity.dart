8f454100df4f4c7f758aa203f386a218460d52ed
Ernesto Carrella <carrknight@gmail.com>
2015-02-25 19:24:57 -0600

    fixed a very subtle error where marshallian quotas would update twice the probability, when fixed the micro model has exactly the same speed of convergence. Yay me!

--- lib/runs/experiments/2015-02-25_productivity.dart
+++ lib/runs/experiments/2015-02-25_productivity.dart
@@ -3,7 +3,6 @@
  * This is open source on MIT license. Isn't this jolly?
  */
 
-
 library experiments.overshoot;
 
 import '2015-02-08 convergenceSpeed.dart';
@@ -17,35 +16,32 @@ import '../yackm.dart';
 main()
 {
 
-
   String timeStamp = new DateTime.now().toString();
 
-  //increase PI parameters of the keynesian controller when the crash comes
+  //increase productivity of the labor force
   for (int i = 0; i < 100; i++)
   {
     print("$i");
+    var directory = ["docs", "yackm", "rawdata", "pi productivity shock${timeStamp}"];
     fixedWageMacro("keynesian.json", pathToJsonFromProjectRoot: ["bin"],
                    additionalJSonFiles : ["shock.experiment.json"],
                    testing:false,
-                   outputPath: ["docs", "yackm", "rawdata", "pi speed shock${timeStamp}"],
-                   gasCsvName: "${i}_sales_flexible.csv",
-                   laborCsvName: "${i}_hr_flexible.csv",
+                   outputPath: directory,
+                   gasCsvName: "${i}_sales_productive.csv",
+                   laborCsvName: "${i}_hr_productive.csv",
                    totalSteps:20000, shockDay:10000, endShockDay:-1, shockSize:-0.2,
                    dateDirectory:false, seed : i, //fix the seed!
                    //increase PI parameter in lieu of speed
                    shockEffects : (OneMarketCompetition scenario,model)
                    {
-                     PIDAdaptive keynesianQuota = scenario.firms.first.purchasesDepartments["labor"].quoting;
-                     PIDController pid = keynesianQuota.pid;
-                     //increase PI by .1
-                     pid.proportionalParameter = pid.proportionalParameter + .2;
-                     pid.integrativeParameter = pid.integrativeParameter + .2;
+                     (scenario.firms.first.plants.first.function as ExponentialProductionFunction).multiplier =
+                     0.6;
                    });
 
     fixedWageMacro("keynesian.json", pathToJsonFromProjectRoot: ["bin"],
                    additionalJSonFiles : ["shock.experiment.json"],
                    testing:false,
-                   outputPath: ["docs", "yackm", "rawdata", "pi speed shock${timeStamp}"],
+                   outputPath: directory,
                    gasCsvName: "${i}_sales.csv",
                    laborCsvName: "${i}_hr.csv.csv",
                    totalSteps:20000, shockDay:10000, endShockDay:-1, shockSize:-0.2,
@@ -53,5 +49,6 @@ main()
 
 
   }
+
 
 }
