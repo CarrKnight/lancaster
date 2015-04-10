f15b9b9c1c5f0da9cbbae9f0f4bcfc0a08453334
Ernesto Carrella <carrknight@gmail.com>
2015-04-09 19:23:44 -0500

    tested (and succeeded) at getting my model in a presentation slide.
    The only issue seems to be that I need to reset before drawing curves (or they get measured wrongly)

---
+++ web/presentation_test/main.dart
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 to Ernesto Carrella.
+ * This is open source on MIT license. Isn't this jolly?
+ */
+
+import 'package:lancaster/view/lancaster_view_angularless.dart';
+import 'dart:html';
+
+void main() {
+
+
+  //need to read the scenario json
+  var path = 'default.json';
+  HttpRequest.getString(path)
+  .then((String fileContents) {
+    print(fileContents);
+    new ProductionDemoGUI.DoubleBeveridge(fileContents,"#demo");
+    //only changes JSON when it comes to fixed production. Pretty cool!
+  })
+  .catchError((Error error) {
+    print(error.toString());
+  });
+
+
+
+}
